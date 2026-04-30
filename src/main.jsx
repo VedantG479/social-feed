d410793f4bb17195a0c1413e3bf396f677815859
VedantG479 <gargvedant2004@gmail.com>
2026-04-30 19:53:20 +0530

    persist data

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,19 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router'
 import UserContextProvider from './context/UserContextProvider'
 import { Provider } from 'react-redux'
-import store from './store/store.js'
+import { store, persistor } from "./store/store.js";
+import { PersistGate } from 'redux-persist/integration/react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
+      <PersistGate loading={<h1>Loading..</h1>} persistor={persistor}>
       <UserContextProvider>
         <BrowserRouter>
           <App />
         </BrowserRouter>
       </UserContextProvider>
+      </PersistGate>
     </Provider>
   </StrictMode>,
 )
--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,35 @@
 import { configureStore } from "@reduxjs/toolkit";
 import postsReducer from './postsSlice'
 import usersReducer from './userSlice'
+import storage from "redux-persist/lib/storage";
+import { persistStore,persistReducer } from "redux-persist";
 
-const store = configureStore({
-    reducer: {
-        posts: postsReducer,
-        users: usersReducer
+console.log(storage)
+const persistConfig = {
+    key: 'root', 
+    storage: storage.default || storage,
+    whitelist: ["posts", "users"]
+}
+
+const rootReducer = {
+    posts: postsReducer,
+    users: usersReducer
+}
+
+const persistedReducer = persistReducer(persistConfig, (state, action) => {
+    return {
+        posts: rootReducer.posts(state?.posts, action), 
+        users: rootReducer.users(state?.users, action)
     }
 })
 
-export default store
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false, 
+    }),
+})
+
+export const persistor = persistStore(store);
